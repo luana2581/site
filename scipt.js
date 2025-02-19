1f37be3e8fd171d94eaf4b7edcd44f3e75b0498f
Felipe <159153862+luana2581@users.noreply.github.com>
2025-02-19 12:34:23 -0300

    19/02

---
+++ scipt.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+    const diminuiFonteBotao = document.getElementById('diminui-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
