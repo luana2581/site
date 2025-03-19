038ceb57d68d85f4faa43a1c4f9bcc6901df3849
Felipe <159153862+luana2581@users.noreply.github.com>
2025-03-19 12:37:04 -0300

    19/03

--- scipt.js
+++ scipt.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById ('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
     botaoDeAcessibilidade.addEventListener('click', function() {
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        botaoDeAcessibilidade.classList.toggle('rotacao-botão');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     });
 });
